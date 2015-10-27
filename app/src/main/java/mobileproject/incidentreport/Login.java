ed61b8eac5c5a3c270731f2dc2c89da092f5b625
kChulEe <kshin33@gmail.com>
2015-10-27 02:06:53 -0400

    Updated login page

--- app/src/main/java/mobileproject/incidentreport/Login.java
+++ app/src/main/java/mobileproject/incidentreport/Login.java
@@ -3,19 +3,19 @@ package mobileproject.incidentreport;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Button;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class Login extends AppCompatActivity {
@@ -36,7 +36,7 @@ public class Login extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Butterknife.bind(this);
+        ButterKnife.bind(this);
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
