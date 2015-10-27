4982c2fdb8af3701d06cf160bc5cb83d7c3e5e86
kChulEe <kshin33@gmail.com>
2015-10-27 00:05:10 -0400

    Initial Upload
    Starting the login page

---
+++ app/src/main/java/mobileproject/incidentreport/Login.java
@@ -0,0 +1,164 @@
+package mobileproject.incidentreport;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+
+public class Login extends AppCompatActivity {
+
+    private static final String TAG = "Login";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Bind(R.id.input_username) EditText _usernameText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.btn_login) Button _loginButton;
+    @Bind(R.id.link_signup) TextView _signupLink;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Butterknife.bind(this);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //begin signing up
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class); startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(Login.this,R.style.AppTheme_PopupOverlay);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String username = _usernameText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        //AUTHENTICATION PROCESS
+
+
+
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        onLoginSucess();    //go to loginsucess if successful call
+                        progressDialog.dismiss();   //if call fails
+                    }
+                }, 3000);
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+                //what happens when signup is successful?
+                this.finish();
+            }
+        }
+    }
+
+    public void onLoginSucess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_LONG).show();
+        _loginButton.setEnabled(true);
+    }
+
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String username = _usernameText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (username.isEmpty()){
+            _usernameText.setError("Enter Username");
+            valid = false;
+        } else {
+            _usernameText.setError(null);
+        }
+
+        if (password.isEmpty()) {
+            _passwordText.setError("Enter Password");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+        return valid;
+    }
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
