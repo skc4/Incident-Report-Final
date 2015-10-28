980923450b01c7ed7cf5e7e3081fac04ec008bde
rettwalker <rettwalker@gmail.com>
2015-10-28 15:15:40 -0400

    got the camera working though it is only doing one picture, which im kinda okay with at the moment. After getting it to save and then send, I will work on getting the current location and after that we can begin uploading incidents and checking that everything works

--- app/src/main/java/mobileproject/incidentreport/Report_A_Incident.java
+++ app/src/main/java/mobileproject/incidentreport/Report_A_Incident.java
@@ -1,9 +1,13 @@
 package mobileproject.incidentreport;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -43,5 +47,23 @@ public class Report_A_Incident extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void takePicture(View view){
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        }
     }
 }
